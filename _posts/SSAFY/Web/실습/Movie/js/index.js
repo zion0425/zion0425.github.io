b62d25ae3afe423ca87a6bc5d9c70c5903df00c0
김시온 <zion0425@naver.com>
2024-03-08 16:46:02 +0900

    feat: 찜하기 기능 추가

--- _posts/SSAFY/Web/실습/Movie/js/index.js
+++ _posts/SSAFY/Web/실습/Movie/js/index.js
@@ -1,5 +1,8 @@
 var buttons = document.querySelectorAll(".movie_favorite > button");
 
+
+let movies_info = [];
+let cnt = 0;
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
         var article = button.closest('article');
@@ -9,12 +12,31 @@ buttons.forEach((button) => {
         var director = article.querySelector('.director').innerText;
         var runningTime = article.querySelector('.runningtime').innerText;
 
-        console.log('영화 제목: ' + name);
-        console.log('장르: ' + genre);
-        console.log('감독: ' + director);
-        console.log('상영 시간: ' + runningTime);
+        let movie_info = {
+            "title": name,
+            "genre": genre,
+            "director": director,
+            "runningTime": runningTime
+        };
+
+        movies_info[movies_info.length] = movie_info;
+
+        let favorite_list = document.getElementById("wish_list");
+
+        localStorage.setItem("movies", JSON.stringify(movies_info));
+        if (cnt >= 1) {
+            favorite_list.innerText = favorite_list.innerText + movie_info.title + " | " + movie_info.genre + " | " + movie_info.director + " | " + movie_info.runningTime + "\n";
+        } else {
+            favorite_list.innerText = movie_info.title + " | " + movie_info.genre + " | " + movie_info.director + " | " + movie_info.runningTime + "\n";
+        }
+        cnt++;
+
+
+        // console.log('영화 제목: ' + name);
+        // console.log('장르: ' + genre);
+        // console.log('감독: ' + director);
+        // console.log('상영 시간: ' + runningTime);
     });
 });
 
 
-    
